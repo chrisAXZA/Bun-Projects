eba40ecda43a7606656f7a38135139387aad4d1d
chrisAXZA <nexter1@tutanota.com>
2022-10-08 12:55:19 +0300

    Implement index.js

---
+++ Zig-WebAssembly-Demo/index.js
@@ -0,0 +1,11 @@
+import fs from 'fs';
+// const fs = require('fs')
+
+const source = fs.readFileSync('web.wasm'); // reads wasm file
+const typedArray = new Uint8Array(source); // source passed to Uint array
+
+// instantiate the webassembly
+WebAssembly.instantiate(typedArray, {})
+    .then((result) => {
+        console.log(result.instance.exports.add(1, 2));
+    });
