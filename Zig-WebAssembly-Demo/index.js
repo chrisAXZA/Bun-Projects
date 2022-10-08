655a3994160d69f13173b1764c26158cf9da693b
chrisAXZA <nexter1@tutanota.com>
2022-10-08 12:57:26 +0300

    Add square functionality to web.zig

--- Zig-WebAssembly-Demo/index.js
+++ Zig-WebAssembly-Demo/index.js
@@ -7,5 +7,5 @@ const typedArray = new Uint8Array(source); // source passed to Uint array
 // instantiate the webassembly
 WebAssembly.instantiate(typedArray, {})
     .then((result) => {
-        console.log(result.instance.exports.add(1, 2));
+        console.log(result.instance.exports.square(2));
     });
--- Zig-WebAssembly-Demo/web.zig
+++ Zig-WebAssembly-Demo/web.zig
@@ -2,4 +2,6 @@ export fn add(n1: i32, n2: i32) i32 {
     return n1 + n2;
 }
 
-
+export fn square(n: i32) i32 {
+    return n * n;
+}
